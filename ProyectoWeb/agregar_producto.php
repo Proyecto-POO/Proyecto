de2e232f907125e37c7e3b5b21a227e7facacc85
John <jkleveronjr@gmail.com>
2016-10-30 16:30:48 -0600

    Nueva Sincronización
    Agregamos el formulario de:
    1. Agregar producto. (juego)
    2. Index Administrador.
    Y agregamos un archivo css para el formulario del administrador.

---
+++ ProyectoWeb/agregar_producto.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agregar Juego</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/hovereffects.css">
+    <link rel="stylesheet" href="css/admin.css">
+
+</head>
+<body style="background-image: url('img/fondo.png'); ">
+		
+	<div class="alert alert-success" role="alert">
+		<a href="index_administrador.php"><button class="btn btn-warning">Volver Atras</button></a>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-lg-offset-3 cont">
+				<table class="table">
+					<tr>
+						<td colspan="2"> 
+							<h4 class="text-center">Nuevo Juego</h4>
+						</td>
+					</tr>
+					<tr>
+						<th>Titulo</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Portada</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Descripcion</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Categoria</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Lanzamiento</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Requisito</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Precio</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<th>Tamano</th>
+						<td><input type="text" name="" class="form-control"></td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<button class="btn btn-primary btn-md form-control">Guardar</button>
+
+						</td>						
+					</tr>
+
+				</table>
+				
+			</div>
+		</div>
+	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<footer>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 col-lg-offset-4">
+                  <img src="img/pago-icono.png" alt="" class="img img-responsive">
+                </div>
+              </div>
+            </div>
+
+            <b><em>GAMERS.ES</em></b> &copy; 2016 - Todos Los Derechos Reservados
+    </footer>
+
+	<script src="js/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+   <script src="js/funciones.js"></script>
+</body>
+</html>
